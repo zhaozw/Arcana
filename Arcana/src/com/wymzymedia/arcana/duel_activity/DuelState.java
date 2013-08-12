8b6562d48064da7ccbd390e35667df0170e18abc
Jygsaw <jygsaw@gmail.com>
2013-08-12 16:30:44 -0700

    Add empty DuelState stub

---
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/DuelState.java
@@ -0,0 +1,15 @@
+package com.wymzymedia.arcana.duel_activity;
+
+import com.wymzymedia.arcana.game_utils.GameState;
+
+public class DuelState extends GameState {
+	public static final String TAG = DuelState.class.getSimpleName();
+
+	// Class variables
+
+	// Helper variables
+
+	// Constructor
+	public DuelState() {
+	}
+}
