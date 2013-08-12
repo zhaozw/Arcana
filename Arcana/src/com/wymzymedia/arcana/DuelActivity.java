09542080f8c8c35860625d1207f432e5cbdc7468
Jygsaw <jygsaw@gmail.com>
2013-08-12 16:29:50 -0700

    Streamline DuelActivity for full screen game display

--- Arcana/src/com/wymzymedia/arcana/DuelActivity.java
+++ Arcana/src/com/wymzymedia/arcana/DuelActivity.java
@@ -1,55 +1,34 @@
 package com.wymzymedia.arcana;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class DuelActivity extends Activity {
+import com.wymzymedia.arcana.duel_activity.DuelView;
+import com.wymzymedia.arcana.game_utils.GameActivity;
+
+public class DuelActivity extends GameActivity {
+	public static final String TAG = DuelActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_duel);
-		// Show the Up button in the action bar.
-		setupActionBar();
-	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void setupActionBar() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
+		// Hide notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.duel, menu);
-		return true;
-	}
+		// Hide title bar
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+		// Lock orientation to portrait
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+		// Set content view
+		gameView = new DuelView(this);
+		setContentView(gameView);
+		Log.d(TAG, "View added");
+	}
 }
