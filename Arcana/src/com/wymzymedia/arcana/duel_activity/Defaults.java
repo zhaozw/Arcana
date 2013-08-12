36153d009c8cefe5159cd725018bbf748863ec46
Jygsaw <jygsaw@gmail.com>
2013-08-12 16:28:22 -0700

    Add Defaults class for Duel activity

---
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/Defaults.java
@@ -0,0 +1,12 @@
+package com.wymzymedia.arcana.duel_activity;
+
+import android.graphics.Color;
+
+public class Defaults {
+	// Interface parameters
+	public static final int BG_COLOR = Color.WHITE;
+	public static final int TEXT_COLOR = Color.BLACK;
+	public static final int PAUSE_DARKEN_COLOR = Color.DKGRAY;
+	public static final int PAUSE_TEXT_COLOR = Color.WHITE;
+
+}
