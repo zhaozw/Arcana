0ac51c093d78b71096d9a016e189e442dcd0438d
Jygsaw <jygsaw@gmail.com>
2013-08-19 05:20:11 -0700

    Implement duel draw card system

---
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelDraw.java
@@ -0,0 +1,35 @@
+package com.wymzymedia.arcana.duel_activity.systems;
+
+import com.wymzymedia.arcana.duel_activity.components.DeckC;
+import com.wymzymedia.arcana.duel_activity.components.VitalsC;
+import com.wymzymedia.arcana.game_utils.GameEntity;
+import com.wymzymedia.arcana.game_utils.GameState;
+import com.wymzymedia.arcana.game_utils.GameSystem;
+
+public class DuelDraw extends GameSystem {
+	public static final String TAG = DuelDraw.class.getSimpleName();
+
+	// Constructor
+	public DuelDraw(GameState state) {
+		super(state);
+
+	}
+
+	// Execute logic on entity
+	@Override
+	protected void execSystem(GameEntity entity) {
+		// retrieve components
+		VitalsC vitals = (VitalsC) entity.getComponent("VitalsC");
+		DeckC newDeck = (DeckC) entity.getComponent("NewDeckC");
+		DeckC handDeck = (DeckC) entity.getComponent("HandDeckC");
+
+		// draw from new deck and add to hand deck
+		int drawNum = vitals.getDrawNum();
+		if (newDeck.getCards().size() < drawNum) {
+			drawNum = newDeck.getCards().size();
+		}
+		if (drawNum > 0) {
+			handDeck.addCards(newDeck.drawTop(drawNum));
+		}
+	}
+}
