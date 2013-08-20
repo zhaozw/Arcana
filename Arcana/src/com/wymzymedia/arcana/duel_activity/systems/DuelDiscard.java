2ef748d6d7ba74e674ba0c95162a900e2f2ce82e
Jygsaw <jygsaw@gmail.com>
2013-08-20 01:26:36 -0700

    Add simple discard logic for played card

--- Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelDiscard.java
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelDiscard.java
@@ -2,6 +2,7 @@ package com.wymzymedia.arcana.duel_activity.systems;
 
 import android.util.Log;
 
+import com.wymzymedia.arcana.duel_activity.components.DeckC;
 import com.wymzymedia.arcana.duel_activity.components.VitalsC;
 import com.wymzymedia.arcana.game_utils.GameEntity;
 import com.wymzymedia.arcana.game_utils.GameState;
@@ -13,6 +14,10 @@ public class DuelDiscard extends GameSystem {
 	// Constructor
 	public DuelDiscard(GameState state) {
 		super(state);
+
+		// set required components
+		addReqComponent("VitalsC");
+		addReqComponent("DiscardDeckC");
 	}
 
 	// Execute logic on entity
@@ -23,8 +28,13 @@ public class DuelDiscard extends GameSystem {
 
 		// retrieve components
 		VitalsC vitals = (VitalsC) entity.getComponent("VitalsC");
+		DeckC discard = (DeckC) entity.getComponent("DiscardDeckC");
 
 		// discard expired cards
+		if (vitals.getCard() >= 0) {
+			discard.addCard(vitals.getCard());
+			vitals.setCard(-1);
+		}
 
 		// progress player to next phase
 		vitals.setPhase(1);
