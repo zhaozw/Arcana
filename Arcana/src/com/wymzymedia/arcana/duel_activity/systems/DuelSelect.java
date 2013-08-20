e30c1e68731e63164bb96098c278e99eda3e9dde
Jygsaw <jygsaw@gmail.com>
2013-08-20 01:21:31 -0700

    Add simple random card select

--- Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelSelect.java
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelSelect.java
@@ -1,7 +1,10 @@
 package com.wymzymedia.arcana.duel_activity.systems;
 
+import java.util.Random;
+
 import android.util.Log;
 
+import com.wymzymedia.arcana.duel_activity.components.DeckC;
 import com.wymzymedia.arcana.duel_activity.components.VitalsC;
 import com.wymzymedia.arcana.game_utils.GameEntity;
 import com.wymzymedia.arcana.game_utils.GameState;
@@ -10,9 +13,16 @@ import com.wymzymedia.arcana.game_utils.GameSystem;
 public class DuelSelect extends GameSystem {
 	public static final String TAG = DuelSelect.class.getSimpleName();
 
+	// Helper variables
+	private final Random rng = new Random();
+
 	// Constructor
 	public DuelSelect(GameState state) {
 		super(state);
+
+		// set required components
+		addReqComponent("VitalsC");
+		addReqComponent("HandDeckC");
 	}
 
 	// Execute logic on entity
@@ -23,13 +33,19 @@ public class DuelSelect extends GameSystem {
 
 		// retrieve components
 		VitalsC vitals = (VitalsC) entity.getComponent("VitalsC");
+		DeckC hand = (DeckC) entity.getComponent("HandDeckC");
 
 		// select card to play
-		if (vitals.isHuman()) {
-			// verify human selection
-		} else {
-			// perform computer selection
+		// TODO verify human selection is valid
+		// if (vitals.isHuman()) {
+		// // verify human selection is valid
+		// } else {
+		// perform computer selection
+		// TODO implement AI strategy instead of random select
+		if (hand.getCards().size() > 0) {
+			vitals.setCard(hand.drawCard(rng.nextInt(hand.getCards().size())));
 		}
+		// }
 
 		// progress player to next phase
 		vitals.setPhase(vitals.getPhase() + 1);
