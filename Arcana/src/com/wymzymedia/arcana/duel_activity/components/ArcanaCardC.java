def200a3b911ecc7b153fb5183754a512cb137ab
Jygsaw <jygsaw@gmail.com>
2013-08-24 05:15:06 -0700

    Add switch for card data to allow initial testing

--- Arcana/src/com/wymzymedia/arcana/duel_activity/components/ArcanaCardC.java
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/components/ArcanaCardC.java
@@ -4,20 +4,39 @@ public class ArcanaCardC extends CardC {
 	public static final String TAG = ArcanaCardC.class.getSimpleName();
 
 	// Class variables
+	private String name;
+	private String phase;
+	private String type;
 
 	// Constructor
 	public ArcanaCardC(int n) {
 		super(n);
 	}
 
+	// Return phase
+	public String getPhase() {
+		return phase;
+	}
+
+	// Return type
+	public String getType() {
+		return type;
+	}
+
+	// Set phase
+	public void setPhase(String p) {
+		phase = p;
+	}
+
+	// Set type
+	public void setType(String t) {
+		type = t;
+	}
+
 	// Return name of card
 	@Override
 	public String getName() {
-		if (getID() >= 0) {
-			return "Arcana:" + getID();
-		} else {
-			return "";
-		}
+		return name;
 	}
 
 	// Load card data
@@ -26,8 +45,29 @@ public class ArcanaCardC extends CardC {
 		setID(n);
 		if (n >= 0) {
 			// set values
+			// TODO implement DB interface
+			switch (n) {
+			case 1:
+				name = "Zap";
+				phase = "play";
+				type = "damage";
+				break;
+			case 2:
+				name = "Heal";
+				phase = "play";
+				type = "heal";
+				break;
+			case 3:
+				name = "Shield";
+				phase = "play";
+				type = "shield";
+				break;
+			}
 		} else {
 			// clear values
+			name = null;
+			phase = null;
+			type = null;
 		}
 	}
 }
