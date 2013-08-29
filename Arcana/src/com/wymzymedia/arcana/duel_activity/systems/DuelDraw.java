cbb789af36cdbe4653be5b461164bbe32a8d3b25
Jygsaw <jygsaw@gmail.com>
2013-08-29 04:47:56 -0700

    Remove automatic 1 to 2 phase progression

--- Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelDraw.java
+++ Arcana/src/com/wymzymedia/arcana/duel_activity/systems/DuelDraw.java
@@ -46,8 +46,5 @@ public class DuelDraw extends GameSystem {
 		if (drawNum > 0) {
 			hand.addCards(draw.drawTop(drawNum));
 		}
-
-		// progress player to next phase
-		vitals.setPhase(vitals.getPhase() + 1);
 	}
 }
